d0f044492c974fd05bd61add1f97bab3d6d994a2
Blake Johnson <blakejohnson@Blakes-MBP.attlocal.net>
2021-06-28 11:56:49 -0500

    added uploadData function to idb.js file

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,25 +1,58 @@
-
 let db;
-const request = indexedDB.open('Budget-Tracker', 1);
+const request = indexedDB.open("Budget-Tracker", 1);
 
-request.onupgradeneeded = function(event) {
-    const db = event.target.result;
-    db.createObjectStore('new_transaction', { autoIncrement: true });
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("new_transaction", { autoIncrement: true });
 };
 
-request.onsuccess = function(event) {
-    db = event.target.result;
-    if (navigator.onLine) {
-        uploadTransaction();
-    }
+request.onsuccess = function (event) {
+  db = event.target.result;
+  if (navigator.onLine) {
+    uploadTransaction();
+  }
 };
 
-request.onerror = function(event) {
-    console.log(event.target.errorCode);
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
 };
 
 function saveRecord(record) {
-    const transaction = db.transaction(['new_transaction'], 'readwrite');
-    const budgetObjectStore = transaction.objectStore('new_transaction');
-    budgetObjectStore.add(record);
-};
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+  const budgetObjectStore = transaction.objectStore("new_transaction");
+  budgetObjectStore.add(record);
+}
+
+function uploadData() {
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+  const transactionObjectStore = transaction.objectStore("new_transaction");
+  const getAll = transactionObjectStore.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      console.log("success!");
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(["new_transaction"], "readwrite");
+          const transactionObjectStore = transaction.objectStore("new_transaction");
+          transactionObjectStore.clear();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+// listen for app coming back online
+window.addEventListener("online", uploadData);
